e5938a174b6dc159339b606d3444db3a374d660c
jjunior112 <juarezgcjr@gmail.com>
2025-09-15 16:58:45 -0300

    Initial commit

---
+++ src/test/java/com/blog_java/BlogJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
