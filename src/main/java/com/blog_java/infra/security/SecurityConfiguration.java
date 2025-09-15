269f6e791a25c240dac8197a5eb6967d8ba35265
jjunior112 <juarezgcjr@gmail.com>
2025-09-15 19:36:32 -0300

    feat: adds `TokenService` ,`SecurityFilter` and `SecurityConfiguration`

---
+++ src/main/java/com/blog_java/infra/security/SecurityConfiguration.java
@@ -0,0 +1,54 @@
+package com.blog_java.infra.security;
+
+import com.blog_java.domain.enums.UserRole;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, SecurityFilter securityFilter) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .cors(cors -> {
+                })
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+
+                        auth
+                                .requestMatchers(HttpMethod.POST, "/users/login","/users/register").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").hasRole(UserRole.ADMIN.name())
+                                .requestMatchers(HttpMethod.POST, "/users/adminRegister").hasRole(UserRole.ADMIN.name())
+                                .requestMatchers(HttpMethod.GET, "/users").hasRole(UserRole.ADMIN.name())
+                                .requestMatchers(HttpMethod.PUT, "/users/**", "users/reactive/**").hasRole(UserRole.ADMIN.name())
+                                .requestMatchers(HttpMethod.DELETE, "/users").hasRole(UserRole.ADMIN.name())
+
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+
+    }
+
+}
