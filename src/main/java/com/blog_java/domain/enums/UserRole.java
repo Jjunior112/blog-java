491a4c6780d5f9a0543727cc9f010c4b892595c6
jjunior112 <juarezgcjr@gmail.com>
2025-09-15 17:00:25 -0300

    feat: adds class `User.java` and enum `UserRole.java`

---
+++ src/main/java/com/blog_java/domain/enums/UserRole.java
@@ -0,0 +1,16 @@
+package com.blog_java.domain.enums;
+
+public enum UserRole {
+    ADMIN("ROLE_ADMIN"),
+    CLIENT("ROLE_CLIENT");
+    private final String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+}
---
+++ src/main/java/com/blog_java/domain/models/User.java
@@ -0,0 +1,82 @@
+package com.blog_java.domain.models;
+
+
+import com.blog_java.domain.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode()
+public class User implements UserDetails {
+    @Id
+    private String id = UUID.randomUUID().toString();
+
+    @Column(unique = true, nullable = false)
+    private String firstName;
+
+    @Column(unique = true, nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    boolean isActive = true;
+
+    public User(String firstName,String lastName, String email, String password, UserRole role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.isActive;
+    }
+}
